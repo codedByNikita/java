9d8fb1fdf6c18e80a5b90466bd395102625fc5fd
Nikita Kumari <nikitasingh98876@gmail.com>
2025-07-23 23:47:53 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+class Add{
+   
+   public static void main(string[] args){
+    
+       int a = Integer.parseInt(args(0));
+       int b = Integer.parseInt(args(1);
+       
+
+	System.out.println(+a);
+	System.out.print(+b);
+}
+
+   private static String args(int i) {
+   
+    throw new UnsupportedOperationException("Unimplemented method 'args'");
+   }
+}
