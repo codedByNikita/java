b3006e6ace6415e42d7a78923c176ce9efcb0176
Nikita Kumari <nikitasingh98876@gmail.com>
2025-07-24 17:27:55 +0530

    Add files via upload

---
+++ kuchbhi.java
@@ -0,0 +1,15 @@
+import pack.calculater;
+
+class kuchbhi
+
+{
+	public static void main(String [] arg)
+	{
+		int a= 10, b = 29;
+		calculater n = new calculater();
+
+		int x = n.add(a,b);
+		System.out.print(x);
+		
+	}
+}
